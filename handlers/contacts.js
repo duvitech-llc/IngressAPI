2cdb84d753bd89234cf79ea245946e99e68f96fe
George Vigelette <gvigelet@duvitech.com>
2018-01-03 11:03:02 -0500

    static data store

--- handlers/contacts.js
+++ handlers/contacts.js
@@ -1,29 +1,12 @@
 'use strict';
-var dataProvider = require('../data/contacts.js');
+
+var contactsDataHandler = require('../data/contactsDataHandler');
+
 /**
  * Operations on /contacts
  */
 module.exports = {
-    /**
-     * summary: 
-     * description: 
-     * parameters: 
-     * produces: application/json, text/json
-     * responses: 200
-     */
-    get: function contacts_get(req, res, next) {
-        /**
-         * Get the data for response 200
-         * For response `default` status 200 is used.
-         */
-        var status = 200;
-        var provider = dataProvider['get']['200'];
-        provider(req, res, function (err, data) {
-            if (err) {
-                next(err);
-                return;
-            }
-            res.status(status).send(data && data.responses);
-        });
+    get: function contacts_get(req, res) {
+        res.json(contactsDataHandler.all())
     }
 };
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var port = process.env.PORT || 8000; // first change
+
 var Http = require('http');
 var Express = require('express');
 var BodyParser = require('body-parser');
 var Swaggerize = require('swaggerize-express');
+var swaggerUi = require('swaggerize-ui'); // second change
 var Path = require('path');
 
 var App = Express();
@@ -16,13 +19,15 @@ App.use(BodyParser.urlencoded({
 }));
 
 App.use(Swaggerize({
-    api: Path.resolve('./config/swagger.json'),
-    handlers: Path.resolve('./handlers')
+    api: Path.resolve('./config/api.json'), // third change
+    handlers: Path.resolve('./handlers'),
+    docspath: '/swagger' // fourth change
 }));
 
-Server.listen(8000, function () {
-    App.swagger.api.host = this.address().address + ':' + this.address().port;
-    /* eslint-disable no-console */
-    console.log('App running on %s:%d', this.address().address, this.address().port);
-    /* eslint-disable no-console */
+// change four
+App.use('/docs', swaggerUi({
+    docs: '/swagger'  
+}));
+
+Server.listen(port, function () { // fifth and final change
 });
